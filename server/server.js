ad8791c54444dc1db04e1995a424e6e7c5a5dc73
Nuttaphon <131658916+Nuttph@users.noreply.github.com>
2024-09-22 23:56:07 +0700

    setting

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
 //for server statement
+const express = require("express");
+const app = express();
+
+const data = [{ message: "Hello world" }];
+
+app.get("/", (req, res) => {
+  res.send(data);
+});
+
+app.listen(5555, () => {
+  console.log("server is running on port : http://localhost:5555");
+});
